06ae9e6763628b58566a3fdf21b7af5d10db8d8b
mhdi-742 <mhgit7427778@gmail.com>
2025-05-13 21:07:53 +0530

    deviation function made

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,27 @@ const mongoose=require('mongoose');
 const { json } = require('stream/consumers');
 router.use(express.json())
 
-
+const getStandardDeviation= (array)=> {
+ let mean=0.0;
+ let sum=0.0;
+ let count=100;
+ for(let i=array.length-1;i>=0 && count>0;i--)
+ {
+    sum+=array[i];
+    count--;
+ }
+ let n=Math.min(100,array.length);
+ mean=sum/n;
+ let ans=0.0;
+ count=100;
+ for(let i=array.length-1;i>=0 && count>0;i--)
+ {
+   ans+=(array[i]-mean)*(array[i]-mean);
+   count--;
+ }
+ ans=ans/(n-1);
+ return ans;
+}
 const storeCryptoStats =async()=>{
     console.log("this is a function");
 
@@ -126,6 +146,38 @@ router.get('/stats',async(req,res)=>{
      }
      res.send(query);
 
+})
+router.get('/diviation',async(req,res)=>{
+  const query=req.body.coin;
+     let data=await CoinData.find({});
+     if(!data.length)
+     {
+        return res.send("Wait");
+     }
+     if(query==="bitcoin")
+     {
+        const ans={
+           deviation:getStandardDeviation(data[0].bitcoin.currentprice)
+        }
+        return res.send(ans);
+     }
+     else if(query==="ethereum")
+     {
+        const ans={
+           deviation:getStandardDeviation(data[0].ethereum.currentprice)
+        }
+        return res.send(ans);
+     }
+     else if(query==="matic-network")
+     {
+      const ans={
+           deviation:getStandardDeviation(data[0].matic_network.currentprice)
+        }
+        return res.send(ans);
+     }
+     else{
+        res.send("wrong query");
+     }
 })
 
 module.exports=router;
