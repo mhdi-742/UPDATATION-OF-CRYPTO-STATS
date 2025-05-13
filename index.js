1feac83f61360b016355de59aa03fefb256eca0e
mhdi-742 <mhgit7427778@gmail.com>
2025-05-14 05:09:07 +0530

    final changes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import { connect, StringCodec } from "nats";
+
+//main function which has the nats publish code.
+
 const main=async()=>{
     const nc=await connect ({
         servers:[
             "localhost:4222"
         ]
     })
-    console.log("Connected to NATS");
+    //console.log("Connected to NATS");
     const sc=StringCodec();
-    nc.publish("pub",sc.encode(JSON.stringify({message:"updte"})));
+    const publishFunction=()=>{
+        nc.publish("pub",sc.encode(JSON.stringify({ trigger: "update" })))
+    };
+    publishFunction();
+    setInterval(publishFunction, 15 *60* 1000);// requests are send every 15 mins
 }
 main();
